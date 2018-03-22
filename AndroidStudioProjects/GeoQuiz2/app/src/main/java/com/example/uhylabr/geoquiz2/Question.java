7ef585e1b4a9ee5c132d0692b4386b1a54b8ea81
UHYLABR <bch2138@columbia.edu>
2018-03-22 13:36:10 -0400

    Version2

--- AndroidStudioProjects/GeoQuiz2/app/src/main/java/com/example/uhylabr/geoquiz2/Question.java
+++ AndroidStudioProjects/GeoQuiz2/app/src/main/java/com/example/uhylabr/geoquiz2/Question.java
@@ -1,8 +1,35 @@
 package com.example.uhylabr.geoquiz2;
 
 /**
- * Created by UHYLABR on 3/11/2018.
+ * Created by UHYLABR on 2/19/2018.
  */
 
 public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId=textResId;
+        mAnswerTrue=answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
 }
+
+
